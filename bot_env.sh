63471cc1c0770e2ec439845f9c647f3822ef3960
Almog Hamdani <almogh52@gmail.com>
2019-09-27 23:47:30 +0300

    Add custom logging + Small refactor

---
+++ bot_env.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+export IG_USERNAME=rddimemes
+export IG_PASSWORD=RedditMemes123
+export SUBREDDITS="memes dankmemes me_irl"
+export TAGS="meme memes funny memesdaily dankmemes lol funnymemes dank like follow humor lmao dankmeme love fortnite anime edgymemes comedy f fun instagram dailymemes offensivememes edgy ol funnymeme cringe haha minecraft bhfyp"
+export RD_USER_AGENT="posix:com.almoghamdani.instabot:v1.0.0 (by /u/almogh52)"
+export RD_CLIENT_ID=3Ih7MLWXCRIzrg
+export RD_CLIENT_SECRECT=SfdheBFKkVybn9ZwE3tWgSqkSSo
+export RD_REFRESH_TOKEN="65194045-infRn9AHuJHxYaakOnHBerrdQp0"
+export SUBREDDIT_POST_FETCH=2
+export FETCH_SUBREDDITS_TIME=120
+export UPLOAD_POSTS_TIME=20
+export POSTS_AMOUNT_PER_UPLOAD=1
+export CLEAN_CACHE_DAYS=3
+export FOLLOW_TAG=memes
+export FOLLOW_AMOUNT_OF_LIKES=10
+
+npm start
